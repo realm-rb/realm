d7d15bddf96e48cfa6f6f9269712431d2b0dba99
David Sevcik <david.sevcik@gmail.com>
2021-05-27 15:19:53 +0200

    Add sns - core integration test

---
+++ realm-sns/spec/realm/integration_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'realm-core'
+require 'realm-sns'
+require 'aws-sdk-core'
+require 'aws-sdk-sns'
+require 'aws-sdk-sqs'
+
+Aws.config.update(endpoint: ENV.fetch('AWS_ENDPOINT'))
+
+module TestIntegrationService
+  module Domain
+    module Submission
+      class PublishCommandHandler < Realm::CommandHandler
+        def handle(submission_id:, **)
+          trigger(:submission_published, submission_id: submission_id)
+        end
+      end
+
+      class SampleEventHandler < Realm::EventHandler
+        inject :event_log
+
+        on :submission_published
+        def handle_submission_published(event)
+          event_log << event
+        end
+      end
+    end
+
+    module Events
+      class SubmissionPublished < Realm::Event
+        body_struct do
+          attribute :submission_id, T::Integer
+        end
+      end
+    end
+  end
+end
+
+RSpec.describe 'Integration of realm SNS with core' do
+  let(:event_log) { [] }
+  let(:dependencies) { { event_log: event_log } }
+  let(:sns) { Aws::SNS::Client.new }
+  let(:sqs) { Aws::SQS::Resource.new }
+  let(:topic_arn) { sns.create_topic(name: 'sample-topic').topic_arn }
+  let(:realm) do
+    Realm.setup(
+      TestIntegrationService,
+      engine_class: nil,
+      prefix: 'integration-test',
+      dependencies: dependencies,
+      event_gateway: { type: :sns, topic_arn: topic_arn, events_module: TestIntegrationService::Domain::Events },
+    ).runtime
+  end
+  let(:worker) { realm.worker(event_processing_attempts: 1) }
+
+  after do
+    sqs.queues.each(&:delete)
+    sns.delete_topic(topic_arn: topic_arn)
+  end
+
+  it 'works for happy path' do
+    worker.start(poller_options: { wait_time_seconds: nil }) # disable wait time to speed up test
+    realm.run('submission.publish', submission_id: 123)
+    worker.stop
+
+    expect(event_log.size).to eq(1)
+    expect(event_log[0]).to be_a(TestIntegrationService::Domain::Events::SubmissionPublished)
+    expect(event_log[0].body.submission_id).to eq(123)
+  end
+end
