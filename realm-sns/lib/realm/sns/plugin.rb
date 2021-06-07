7e28b8e50f66b5847bfbed28d6deaf5924f062fa
David Sevcik <david.sevcik@gmail.com>
2021-06-07 16:33:00 +0200

    Remove unnecessary code

--- realm-sns/lib/realm/sns/plugin.rb
+++ realm-sns/lib/realm/sns/plugin.rb
@@ -6,8 +6,6 @@ require_relative 'gateway'
 module Realm
   module SNS
     class Plugin < Realm::Plugin
-      name :sns
-
       def self.setup(_config, container)
         container.register('event_router.gateway_classes.sns', SNS::Gateway)
       end
