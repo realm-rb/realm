8f77d51c60e9f7bd017de999938dde618d223090
David Sevcik <david.sevcik@gmail.com>
2021-07-26 11:20:29 +0200

    Separate versionning for each gem

--- bin/publish-gems.sh
+++ bin/publish-gems.sh
@@ -2,11 +2,11 @@
 
 all_gems=(realm-core realm-elasticsearch realm-rom realm-sns)
 gems_to_publish=${1:-${all_gems[@]}}
-version=$(<VERSION)
 root=$(pwd)
 
 for name in $gems_to_publish; do
   cd "${root}/${name}"
+  version=$(<VERSION)
   gem build "${name}.gemspec"
   gem push "${name}-${version}.gem"
   rm "${name}-${version}.gem"
